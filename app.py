3c942ea71b124086847ca5f0962096e26372ba34
ONAFOWOKAN TESTAMENT <101755032+Onafowokan-Testament@users.noreply.github.com>
2025-02-03 13:11:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ file_name = "vector_index.pkl"
 st.title("Covenant University Student Book Chatbot")
 st.sidebar.title("Chat with me")
 
-
-process_url_clicked = st.sidebar.button("Process Handbook")
+if not os.path.exists(file_name):
+    process_url_clicked = st.sidebar.button("Process Handbook")
 
 
 def split_into_chunks(pages):
